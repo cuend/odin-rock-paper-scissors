6cd48491edffda4ee96d30741b1e1d0b0b7ed489
David Cuen <dccuen@gmail.com>
2023-09-14 18:58:29 -0700

    added round functionality
    playRound function plays a round of Rock Paper Scissors
    based on the user input and a random selection

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function getComputerChoice () {
+function getComputerChoice() {
     // initialize a random number between 0 and 1
     const randomNum = Math.random();
 
@@ -20,4 +20,70 @@ function getComputerChoice () {
     }
 };
 
-console.log(getComputerChoice());
+function playRound(playerSelection, computerSelection) {
+    // format player's selection
+    playerSelection = playerSelection.toLowerCase();
+    
+    // based on the players selection we have 3 paths
+    if (playerSelection === "rock")
+    {
+        // selections based on rock
+        switch (computerSelection) {
+            case "rock":
+                return "You Tie! Rock versus Rock";
+            
+            case "paper":
+                return "You Lose! Paper beats Rock";
+
+            case "scissors":
+                return "You Win! Rock beats Scissors";
+        
+            default:
+                break;
+        }
+    }
+    else if (playerSelection === "paper")
+    {
+        // selections based on Paper
+        switch (computerSelection) {
+            case "rock":
+                return "You Win! Paper beats Rock";
+            
+            case "paper":
+                return "You Tie! Paper versus Paper";
+
+            case "scissors":
+                return "You Lose! Scissors beats Paper";
+        
+            default:
+                break;
+        }
+    }
+    else if (playerSelection === "scissors")
+    {
+        // selections based on Scissors
+        switch (computerSelection) {
+            case "rock":
+                return "You Lose! Rock beats Scissors";
+            
+            case "paper":
+                return "You Win! Scissors beats Paper";
+
+            case "scissors":
+                return "You Tie! Scissors versus Scissors";
+        
+            default:
+                break;
+        }
+    }
+    else
+    {
+        return "Invalid selection";
+    }
+};
+
+let p1 = "scissors";
+let com = getComputerChoice();
+
+console.log(com);
+console.log(playRound(p1, com));
